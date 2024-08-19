f40a91ddcfdb3c2dbe176443bb2baf1709bf4681
Abdul Moeed <moeedali806@gmail.com>
2024-08-19 16:26:57 +0500

    First

---
+++ src/scientificcalculator/ScientificCalculator.java
@@ -0,0 +1,12 @@
+
+package scientificcalculator;
+
+public class ScientificCalculator {
+
+    public static void main(String[] args) {
+//       ScientificCalc c = new ScientificCalc();
+       
+       
+    }
+    
+}
